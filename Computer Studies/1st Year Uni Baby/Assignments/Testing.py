1591039b8d3741bf3fd03f060f3e2944d0706be4
Nick Dang <nhan25032004@gmail.com>
2022-10-21 09:50:14 -0400

    changed surface (no blit()), test amount of points

--- Computer Studies/1st Year Uni Baby/Assignments/Testing.py
+++ Computer Studies/1st Year Uni Baby/Assignments/Testing.py
@@ -14,20 +14,21 @@ image = pygame.image.load(a)
 
 #get image height & width
 (image_width,image_height) = image.get_size()
-screen = pygame.display.set_mode((image_width*5, image_height*5)) #upscale drawn image by 5
+screenPoints = pygame.display.set_mode((image_width*5, image_height*5)) #upscale drawn image by 5
+
 
 #put image onto window surface
-screen.blit(image,(0,0))
+#screen.blit(image,(0,0))
 
 #check every single pixel of source image
 for y in range(image_height):
    for x in range(image_width):
-        (r, g, b, _) = screen.get_at((x, y)) #check RGB value of pixel
+        (r, g, b, _) = image.get_at((x, y)) #check RGB value of pixel
         
         #cacl # of circles for red, green and blue
-        a = int(r/70)
-        c = int(g/70)
-        d = int(b/70)
+        a = int(r/60)
+        c = int(g/60)
+        d = int(b/60)
 
         if x == 0 and y ==0: #check left corner pixel
             if (r,g, b, _) == (250,45, 208): #check if that pixel is magenta
@@ -35,30 +36,30 @@ for y in range(image_height):
                 c = int(g/70)
                 d = int(b/40)
                 while a > 0:
-                    pygame.draw.circle(screen, (255,0,0),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
+                    pygame.draw.circle(screenPoints, (255,0,0),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
                     a -=1
                 while c > 0:
-                    pygame.draw.circle(screen, (0,255,0),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
+                    pygame.draw.circle(screenPoints, (0,255,0),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
                     c -=1
                 while d > 0:
-                    pygame.draw.circle(screen, (0,0,255),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
+                    pygame.draw.circle(screenPoints, (0,0,255),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
                     d -=1
         else: #otherwise draw as normal 
             while a > 0:
-                pygame.draw.circle(screen, (255,0,0),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
+                pygame.draw.circle(screenPoints, (255,0,0),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
                 a -=1
             while c > 0:
-                pygame.draw.circle(screen, (0,255,0),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
+                pygame.draw.circle(screenPoints, (0,255,0),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
                 c -=1
             while d > 0:
-                pygame.draw.circle(screen, (0,0,255),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
+                pygame.draw.circle(screenPoints, (0,0,255),(random.randint((x*5)-15,x*5),random.randint((y*5)-15,y*5)),1)
                 d -=1
 
   
 
 pygame.display.update()# update drawn points onto screen
 
-pygame.image.save(screen,"suqintingDrawing.png") #save image
+pygame.image.save(screenPoints,"suqintingDrawing.png") #save image
 
 
 while True:
