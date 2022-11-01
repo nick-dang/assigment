3048fcb6c82d4026d898f34df4353f4ebc69d4f9
Khoi To <khoito100@gmail.com>
2022-11-01 00:28:26 -0400

    FINISHED

--- Computer Studies/1st Year Uni Baby/Assignments/comp1405_f22_101286968_assignment_06.py
+++ Computer Studies/1st Year Uni Baby/Assignments/comp1405_f22_101286968_assignment_06.py
@@ -17,6 +17,8 @@ def grid():
         for j in range(7): #for y-axis
             squares.append(pygame.draw.rect(screen, (0,0,0), (i*squareWidth, j*squareHeight, squareWidth,squareHeight), width=1))
 
+
+
 #window & grid dimension
 squareWidth = 80
 squareHeight= 80
@@ -46,49 +48,40 @@ player1Direction = True #check status depending on whether the players are movin
 player2Direction = True
 playerTurn = True #switch between players, player 1 (True) & player 2 (false)
 
-while ply1_x_pos [1] != squares[49] or ply2_x_pos [1] != squares[49]: #check if players reach the end of board game
-    pygame.time.delay(500)
+while True: #check if players reach the end of board game
+    pygame.time.delay(700)
     screen.fill((255,255,255))
-    pygame.draw.rect(screen, (255,0,0), (ply1_x_pos[0], ply1_y_pos[0], 25, 25))
-    pygame.draw.rect(screen, (0,255,0), (ply2_x_pos[0], ply2_y_pos[0], 25, 25))
+
+    
     #loop to draw the board game
     grid()
-    
     
     #player 1 move
     if playerTurn == True: 
         diceValue = die() #roll dice
-        print(diceValue)
-        print(player1Direction)
-
+        print("Player 1's dice value is", diceValue)
+        
         if player1Direction == True: #when players moving right
-            player1SquaresLeft = int((squares[player1End][0] - squares[player1CurrentPos][0])/80)
-            
-            if ply1_x_pos[1] == squares[20]:
-                player1CurrentPos = 32
-                ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]]
-
+            player1SquaresLeft = int((squares[player1End][0] - squares[player1CurrentPos][0])/80) #get amount of squares left per row
 
             if ply1_x_pos[0] in range(0, 560) and ply1_y_pos[0] == 20 and (player1CurrentPos +7*diceValue)  > 49: #check that current position will have to roll the exact squares left to land on the finish line
-                
                 playerTurn = False
+
             elif diceValue <= player1SquaresLeft: #check if dice value is < than number of squares left
-                
                 if (player1CurrentPos+7*diceValue) <= player1End: #check if current position will move out of bound if move to the dice value 
                     player1CurrentPos += 7*diceValue#move the current position to the dice value 
-                    print("player1 current pos", player1CurrentPos)
+                    
                     ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]] #move player 1
                     playerTurn = False #switch player
             
             elif diceValue> player1SquaresLeft: #check if dice value is > # of squares left
                 
-                print("ply1 SquaresLeft", player1SquaresLeft)
                 player1CurrentPos += 7*player1SquaresLeft #update location and move all the way to right side
                 playerTurn = False
 
-                print(player1CurrentPos, player1End)
+                
                 if player1CurrentPos == player1End: #check if player1 touch the border square
-                    player1End -= 2
+                    player1End -= 2 #minus the right border
                     
                     player1SquaresLeft = diceValue- player1SquaresLeft -1 #amount of move left after move up 1 row
 
@@ -98,25 +91,26 @@ while ply1_x_pos [1] != squares[49] or ply2_x_pos [1] != squares[49]: #check if 
                     ply1_y_pos = [squares[player1CurrentPos][1]+20, squares[player1CurrentPos]] #move up 1 row
                     ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]]
                     playerTurn = False
-                    print("player1 current pos", player1CurrentPos)
+                    
                     player1Direction = False
+        
 
         elif player1Direction == False: #when player 1 move left
             player1SquaresLeft = int((squares[player1CurrentPos][0] - squares[player1LeftColumn][0])/80)
-            print("ply1 SquaresLeft", player1SquaresLeft)
+
             if diceValue<= player1SquaresLeft: #check if dice value is < than number of squares left
                 if (player1CurrentPos-7*diceValue) >= player1LeftColumn:
                     player1CurrentPos -= 7*diceValue
-                    print("player1 current pos", player1CurrentPos)
+                    
                     ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]]
                     playerTurn = False
                 
             elif diceValue> player1SquaresLeft:
-                print("ply1 SquaresLeft", player1SquaresLeft)
+                
                 player1CurrentPos -= 7*player1SquaresLeft #update location and move all the way to left side
                 playerTurn = False
                 
-                print("ply1CurrentPos & ply1LeftColumn", player1CurrentPos, player1LeftColumn)
+                
                 
                 if player1CurrentPos == player1LeftColumn: #check if player1 touch the border square
                     player1LeftColumn -= 2
@@ -129,33 +123,72 @@ while ply1_x_pos [1] != squares[49] or ply2_x_pos [1] != squares[49]: #check if 
                     ply1_y_pos = [squares[player1CurrentPos][1]+20, squares[player1CurrentPos]] #move up 1 row
                     ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]]
                     playerTurn = False
-                    print("player1 current pos", player1CurrentPos)
+                    
                     player1Direction = True
         
+        #ladders
+        if ply1_x_pos[1] == squares[20]:
+            player1CurrentPos = 32
+            ply1_y_pos = [squares[player1CurrentPos][1]+20, squares[player1CurrentPos]] #move up 1 row
+            ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]]
+            player1End -= 2
+            player1LeftColumn -= 2
+            print("YOU STEP ON A LADDER AT  20")
+        elif ply1_x_pos[1] == squares[11]:
+            player1CurrentPos = 30
+            ply1_y_pos = [squares[player1CurrentPos][1]+20, squares[player1CurrentPos]] #move up 1 row
+            ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]]
+            player1End -= 2
+            player1LeftColumn -= 2
+            print("YOU STEP ON A LADDER AT  11")
+        elif ply1_x_pos[1] == squares[44]:
+            player1CurrentPos = 35
+            ply1_y_pos = [squares[player1CurrentPos][1]+20, squares[player1CurrentPos]] #move up 1 row
+            ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]]
+            player1End -= 2
+            player1LeftColumn -= 2
+            print("YOU STEP ON A LADDER AT  44")
+        #snakes
+        elif ply1_x_pos[1] == squares[18]: 
+            player1CurrentPos = 13
+            ply1_y_pos = [squares[player1CurrentPos][1]+20, squares[player1CurrentPos]] #move up 1 row
+            ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]]
+            player1LeftColumn += 2
+            player1End += 2
+            print("YOU STEP ON A SNAKES AT  18")
+        elif ply1_x_pos[1] == squares[21]:
+            player1CurrentPos = 16
+            ply1_y_pos = [squares[player1CurrentPos][1]+20, squares[player1CurrentPos]] #move up 1 row
+            ply1_x_pos = [squares[player1CurrentPos][0]+20, squares[player1CurrentPos]]
+            player1LeftColumn += 2
+            player1End += 2
+            print("YOU STEP ON A SNAKES AT  21")
+        
+        
+
     #player 2 move
     if playerTurn == False: 
         diceValue = die() #roll dice
-        print(diceValue)
-        print(player2Direction)
-
+        print("Player 2's dice value is", diceValue)
+        
         if player2Direction == True: #when players moving right
             player2SquareLeft = int((squares[player2End][0] - squares[player2CurrentPos][0])/80)
             
             if ply2_x_pos[0] in range(0, 560) and ply2_y_pos[0] == 40 and (player2CurrentPos+7*diceValue)  > 49: #check that current position will have to roll the exact squares left to land on the finish line
                 
                 playerTurn = True
-            elif diceValue<= player2SquareLeft: #check if dice value is < than number of squares left
+            elif diceValue <= player2SquareLeft: #check if dice value is < than number of squares left
                 if (player2CurrentPos+7*diceValue) <= player2End: #check if current position will move out of bound if move to the dice value 
                     player2CurrentPos += 7*diceValue#move the current position to the dice value 
-                    print("player2 current pos", player2CurrentPos)
+                    
                     ply2_x_pos = [squares[player2CurrentPos][0]+40, squares[player2CurrentPos]] #move player 1
                     playerTurn = True
             
             elif diceValue > player2SquareLeft: #check if dice value is > # of squares left
-                print("player2 squaresLeft", player2SquareLeft)
+                
                 player2CurrentPos += 7*player2SquareLeft #update location and move all the way to right side
                 
-                print(player2CurrentPos, player2End)
+                
 
                 if player2CurrentPos == player2End: #check if player1 touch the border square
                     player2End -= 2
@@ -167,26 +200,26 @@ while ply1_x_pos [1] != squares[49] or ply2_x_pos [1] != squares[49]: #check if 
                     ply2_y_pos = [squares[player2CurrentPos][1]+40, squares[player2CurrentPos]] #move up 1 row
                     ply2_x_pos = [squares[player2CurrentPos][0]+40, squares[player2CurrentPos]]
                     playerTurn = True
-                    print("player2 current pos", player2CurrentPos)
+                    
                     player2Direction = False
 
 
         elif player2Direction == False: #when player 2 move left
             player2SquareLeft = int((squares[player2CurrentPos][0] - squares[player2LeftColumn][0])/80)
-            print("player2 squaresLeft", player2SquareLeft)
+            
             if diceValue<= player2SquareLeft: #check if dice value is < than number of squares left
                 if (player2CurrentPos-7*diceValue) >= player2LeftColumn:
                     player2CurrentPos -= 7*diceValue
-                    print("player2 current pos", player2CurrentPos)
+                    
                     ply2_x_pos = [squares[player2CurrentPos][0]+40, squares[player2CurrentPos]]
                     playerTurn = True
 
             elif diceValue > player2SquareLeft:
-                print("player2 squaresLeft", player2SquareLeft)
+                
                 player2CurrentPos -= 7*player2SquareLeft #update location and move all the way to left side
 
                 
-                print("ply2Pos & ply2LeftColumn", player2CurrentPos, player2LeftColumn)
+                
                 if player2CurrentPos == player2LeftColumn: #check if player1 touch the border square
                     player2LeftColumn -= 2 #subtract left border by 2
 
@@ -195,15 +228,55 @@ while ply1_x_pos [1] != squares[49] or ply2_x_pos [1] != squares[49]: #check if 
                     ply2_y_pos = [squares[player2CurrentPos][1]+40, squares[player2CurrentPos]] #move up 1 row
                     ply2_x_pos = [squares[player2CurrentPos][0]+40, squares[player2CurrentPos]]
                     playerTurn = True
-                    print("player2 current pos", player2CurrentPos)
+                    
                     player2Direction = True
 
+        #ladders
+        if ply2_x_pos[1] == squares[20]:
+            player2CurrentPos = 32
+            ply2_y_pos = [squares[player2CurrentPos][1]+20, squares[player2CurrentPos]] #move up 1 row
+            ply2_x_pos = [squares[player2CurrentPos][0]+20, squares[player2CurrentPos]]
+            player2End -= 2
+            player2LeftColumn -= 2
+            print("YOU STEP ON A LADDER AT 20")
+        elif ply2_x_pos[1] == squares[11]:
+            player2CurrentPos = 30
+            ply2_y_pos = [squares[player2CurrentPos][1]+20, squares[player2CurrentPos]] #move up 1 row
+            ply2_x_pos = [squares[player2CurrentPos][0]+20, squares[player2CurrentPos]]
+            player2End -= 2
+            player2LeftColumn -= 2
+            print("YOU STEP ON A LADDER AT  11")
+        elif ply2_x_pos[1] == squares[44]:
+            player2CurrentPos = 35
+            ply2_y_pos = [squares[player2CurrentPos][1]+20, squares[player2CurrentPos]] #move up 1 row
+            ply2_x_pos = [squares[player2CurrentPos][0]+20, squares[player2CurrentPos]]
+            player2End -= 2
+            player2LeftColumn -= 2
+            print("YOU STEP ON A LADDER AT  44")
+        #snakes
+        elif ply2_x_pos[1] == squares[18]: 
+            player2CurrentPos = 13
+            ply2_y_pos = [squares[player2CurrentPos][1]+20, squares[player2CurrentPos]] #move up 1 row
+            ply2_x_pos = [squares[player2CurrentPos][0]+20, squares[player2CurrentPos]]
+            player2LeftColumn += 2
+            player2End += 2
+            print("YOU STEP ON A SNAKES AT  18")
+        elif ply2_x_pos[1] == squares[21]:
+            player2CurrentPos = 16
+            ply2_y_pos = [squares[player2CurrentPos][1]+20, squares[player2CurrentPos]] #move up 1 row
+            ply2_x_pos = [squares[player2CurrentPos][0]+20, squares[player2CurrentPos]]
+            player2LeftColumn += 2
+            player2End += 2
+            print("YOU STEP ON A SNAKES AT  21")
+        
 
-
-   
     
+   
+    pygame.draw.rect(screen, (255,0,0), (ply1_x_pos[0], ply1_y_pos[0], 25, 25))
+    pygame.draw.rect(screen, (0,255,0), (ply2_x_pos[0], ply2_y_pos[0], 25, 25))
     pygame.display.update()
-
+    if ply1_x_pos[1] == squares[49] or ply2_x_pos[1] == squares[49]: #whenever player 1 or player 2 reaches the end, game stops 
+            break
 
 
 
