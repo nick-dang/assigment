bec7990b017de1f79ffc9189f0bab583a97f5f5d
Nick Dang <nhan25032004@gmail.com>
2022-12-06 15:31:50 -0500

    fixed

--- Computer Studies/1st Year Uni Baby/Tutorial/comp1405_f22_101286968_tutorial_07_a.py
+++ Computer Studies/1st Year Uni Baby/Tutorial/comp1405_f22_101286968_tutorial_07_a.py
@@ -6,34 +6,38 @@
 
 def check_matrix(matrix):
     list_to_check_matrix = [] #a list to keep track that every requirement of "valid matrix" is met
-    if True in list_to_check_matrix:
-        for row in matrix: #check every row in that matrix is a list
-            if isinstance(row,list) == True:
-                list_to_check_matrix.append(True)
-               
-            else:
-                list_to_check_matrix.append(False)
-
-        next_row = 0
-        for row in range(len(matrix)): #check every row is the length
-            next_row = row +1
-            if next_row >= len(matrix): #check if next row is out of index of list 
-                break 
-            
-            if len(matrix[row]) == len(matrix[next_row]): #check if current row and next row is same length
-                list_to_check_matrix.append(True) 
-            
-            else:
-                list_to_check_matrix.append(False)
-        
-        for row in range(len(matrix)): #check every element in the matrix is numeric
-            for val in matrix[row]:
-                if isinstance(val,int) == True or isinstance(val,float) == True:
-                    list_to_check_matrix.append(True)
-                else:
-                    list_to_check_matrix.append(False)
-
     
-    else:
-        return False #meaning matrix isn't valid
+    for row in matrix: #check every row in that matrix is a list
+        if isinstance(row,list) == True:
+            list_to_check_matrix.append(True)
+            
+        else:
+            list_to_check_matrix.append(False)
+
+    next_row = 0
+    for row in range(len(matrix)): #check every row is the length
+        next_row = row +1
+        if next_row >= len(matrix): #check if next row is out of index of list 
+            break 
         
+        if len(matrix[row]) == len(matrix[next_row]): #check if current row and next row is same length
+            list_to_check_matrix.append(True) 
+        
+        else:
+            list_to_check_matrix.append(False)
+    
+    for row in range(len(matrix)): #check every element in the matrix is numeric
+        for val in matrix[row]:
+            if isinstance(val,int) == True or isinstance(val,float) == True:
+                list_to_check_matrix.append(True)
+            else:
+                list_to_check_matrix.append(False)
+
+    if False in list_to_check_matrix:
+        return False
+    else:
+        return True #meaning matrix isn't valid
+        
+
+if check_matrix([[1,2,3],[4,5,6]]) == True:
+    print("hello")
